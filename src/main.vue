92fc592170858d364f955fb9050b90e7deb0d44d
Wagner Andrade <19WAS85+github@gmail.com>
2019-09-02 20:25:18 -0300

    Fix logo path to use relative subdir

--- src/main.vue
+++ src/main.vue
@@ -3,7 +3,7 @@
     <header class="w-full px-4 pt-2 pb-1">
       <h1>
         <a href="/" class="inline-block align-bottom opacity-50">
-          <img class="w-8 mr-2 inline-block" src="/open-book.png">
+          <img class="w-8 mr-2 inline-block" src="open-book.png">
           <span class="inline-block align-bottom font-light text-lg">
             Checkpoints
             <small class="text-xs align-middle">
