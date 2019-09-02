04f150d23fa8e24f06666e8cdca1db995d9d6744
Wagner Andrade <19WAS85+github@gmail.com>
2019-09-02 19:21:42 -0300

    Add version to header

--- src/main.vue
+++ src/main.vue
@@ -2,10 +2,13 @@
   <section :id="$options.name" class="container mx-auto">
     <header class="w-full px-4 pt-2 pb-1">
       <h1>
-        <a href="/" class="inline-block align-middle opacity-50">
+        <a href="/" class="inline-block align-bottom opacity-50">
           <img class="w-8 mr-2 inline-block" src="/open-book.png">
-          <span class="inline-block align-middle font-light text-lg">
+          <span class="inline-block align-bottom font-light text-lg">
             Checkpoints
+            <small class="text-xs align-middle">
+              ({{ version }})
+            </small>
           </span>
         </a>
       </h1>
@@ -15,11 +18,13 @@
 </template>
 
 <script>
+import { version } from '../package.json'
 import NotesDashboard from '@/components/notes-dashboard'
 
 export default {
   name: 'checkpoints-main',
-  components: { NotesDashboard }
+  components: { NotesDashboard },
+  data () { return { version } }
 }
 </script>
 
