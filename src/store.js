3a04faeef813d6e6d7f31138d4f78898d02a3d84
Wagner Andrade <19WAS85+github@gmail.com>
2019-09-02 21:49:02 -0300

    Fix start error link

--- src/store.js
+++ src/store.js
@@ -17,7 +17,7 @@ const save = (key, obj) => localStorage.setItem(key, JSON.stringify(obj))
 
 const start = () => {
   return [
-    parse({ title: ' Getting Started', text: 'Hello, welcome to #Checkpoints, your personal #knowledge wiki!\nEdit and list here the start content, it will be your home page.\n - Tips and treats here: #Checkpoints;\n - https://github.com/19WAS85/checkpoints;' }),
+    parse({ title: ' Getting Started', text: 'Hello, welcome to #Checkpoints, your personal #knowledge wiki!\nEdit and list here the start content, it will be your home page.\n - Tips and treats here: #Checkpoints;\n - https://github.com/19WAS85/checkpoints' }),
     parse({ title: 'Checkpoints', text: '> Client hash-link based personal wiki using markdown and local-storage.\n### Tricks\n - Create rich text, list, images, tables and others using [markdown](https://guides.github.com/features/mastering-markdown/);\n - Click or #create simple `#hash` or `[Markdown Links](#hash)` to create related content;\n - Archive is character sorted, use prefixes to group them;\n - Double click at item header is a shortcut to edit;\n - The first archive item is your default home;' })
   ]
 }
