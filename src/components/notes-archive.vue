22a3f03a8c1daced7eb2c1ded7373d2216408fc5
Wagner Andrade <19WAS85+github@gmail.com>
2019-09-17 18:02:00 -0300

    Add notes search

--- src/components/notes-archive.vue
+++ src/components/notes-archive.vue
@@ -13,8 +13,14 @@
         </button>
       </div>
     </header>
+    <input
+      class="border border-gray-200 rounded mt-2 py-1 px-2 w-full text-xs"
+      type="text"
+      placeholder="Search"
+      onfocus="this.select()"
+      v-model="query">
     <ul class="mt-2 ml-2 list-disc list-inside text-sm">
-      <li v-for="note in notes" :key="note.key">
+      <li v-for="note in search(query, notes)" :key="note.key">
         <a class="cursor-pointer text-blue-500"
           :href="`#${note.key}`" @click="$emit('select', note)">
           {{ note.title }}
@@ -27,6 +33,18 @@
 <script>
 export default {
   name: 'notes-archive',
-  props: { notes: Array }
+  props: { notes: Array },
+  methods: {
+    search (query, notes) {
+      if (!notes) return
+      if (query.length === 0) return notes
+      return notes.filter(note => {
+        const lowerQuery = query.toLowerCase()
+        return note.title.toLowerCase().indexOf(lowerQuery) >= 0 ||
+          note.text.toLowerCase().indexOf(lowerQuery) >= 0
+      })
+    }
+  },
+  data () { return { query: '' } }
 }
 </script>
